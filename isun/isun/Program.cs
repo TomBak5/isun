7dbdd00f84c577df8099924225308a7439f4a33e
DgTm <email>
2023-12-01 16:00:04 +0200

    Stop execution if no params or first param is wrong

--- isun/isun/Program.cs
+++ isun/isun/Program.cs
@@ -7,6 +7,7 @@ namespace isun
     {
         static void Main(string[] args)
         {
+
             var builder = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
                                          .AddJsonFile("appsettings.json");
 
@@ -15,6 +16,18 @@ namespace isun
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(config)
                 .CreateLogger();
+
+            if (args.Length == 0)
+            {
+                Log.Logger.Warning("No params. Exiting..");
+                return;
+            }
+
+            if (args[0] != "--cities")
+            {
+                Log.Logger.Warning("First param is wrong. Shoud be \"--cities\"");
+                return;
+            }
         }
     }
 }
