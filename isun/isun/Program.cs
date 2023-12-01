2e32fb0cf0e9ef3d79bca8ca2037a018a3ce01bf
DgTm <email>
2023-12-01 15:48:25 +0200

    Add logging

--- isun/isun/Program.cs
+++ isun/isun/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace isun
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace isun
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            var builder = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                                         .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
         }
     }
 }
