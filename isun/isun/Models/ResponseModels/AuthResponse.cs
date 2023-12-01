883c814025f2dfd17b76a89029aa4233ece7d2e0
DgTm <email>
2023-12-01 16:18:59 +0200

    Add request and response models

---
+++ isun/isun/Models/ResponseModels/AuthResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace isun.Responses
+{
+    public class AuthResponse
+    {
+        [JsonPropertyName("token")]
+        public string TokenValue { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            return this.TokenValue == ((AuthResponse)obj).TokenValue;
+        }
+    }
+}
---
+++ isun/isun/Models/ResponseModels/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace isun.Responses
+{
+    public class Response<TRespData> where TRespData : class
+    {
+        public bool IsOk { get; set; }
+        public TRespData Object { get; set; }
+        public ProblemDetails Error { get; set; }
+    }
+}
